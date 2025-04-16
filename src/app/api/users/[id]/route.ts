b827d20b17077db117972d4f08c753e16bc513c2
Barbara Poblete <bpobletem96@gmail.com>
2025-04-16 16:12:54 -0400

    endpoint get user by id

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
+
+  if (!id) {
+    return NextResponse.json({ error: 'ID not found' }, { status: 404 });
+  }
+  
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+    });
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return NextResponse.json({ error: 'Failed to fetch user' }, { status: 500 });
+  }
+}
