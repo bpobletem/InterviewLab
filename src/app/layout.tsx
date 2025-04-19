3fba7fa2135bf114db0db25d79b2ed88bc3283c3
Barbara Poblete <bpobletem96@gmail.com>
2025-04-19 15:22:47 -0400

    borrar next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "./providers/AuthProvider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AuthProvider>
+      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
           {children}
-        </AuthProvider>
       </body>
     </html>
   )
