e3364ce2f33f4affc2d93157d94c3501c47b12c2
Barbara Poblete <bpobletem96@gmail.com>
2025-04-21 14:56:12 -0400

    edit navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,28 +2,34 @@
 
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@/utils/supabase/client'
 
 export default function Navbar() {
-    const router = useRouter()
+  const router = useRouter()
+  const supabase = createClient()
 
-    const handleLogout = async () => {
-        await supabase.auth.signOut()
-        router.push('/login')
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+      console.error('[Header] Error signing out:', error);
+      return;
     }
+    router.push('/')
+  }
 
-    return (
-        <nav className="w-full border-b border-gray-200 px-6 py-3 flex justify-between items-center bg-white text-sm">
-            <Link href="/home" className="font-semibold text-gray-800">InterviewLab</Link>
-            <div className="flex items-center gap-4">
-                <Link href="/home" className="text-gray-600 hover:text-black transition">Inicio</Link>
-                <button
-                    onClick={handleLogout}
-                    className="text-gray-500 hover:text-black transition hover:cursor-pointer"
-                >
-                    Cerrar sesión
-                </button>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="w-full border-b border-gray-200 px-6 py-3 flex justify-between items-center bg-white text-sm">
+      <Link href="/home" className="font-semibold text-gray-800">InterviewLab</Link>
+      <div className="flex items-center gap-4">
+        <Link href="/home" className="text-gray-600 hover:text-black transition">Inicio</Link>
+        <button
+          onClick={handleLogout}
+          className="text-gray-500 hover:text-black transition hover:cursor-pointer"
+        >
+          Cerrar sesión
+        </button>
+      </div>
+    </nav>
+  )
 }
