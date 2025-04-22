abb75c40269f5c225c725816aa92d2b4a4cabaf0
Barbara Poblete <bpobletem96@gmail.com>
2025-04-22 13:53:19 -0400

    eliminar tipos que no se usan

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,46 +1,3 @@
-// User types
-export interface User {
-  id: string;
-  name: string;
-}
-
-// Institution types
-export interface Institution {
-  id: bigint;
-  name: string;
-  email: string;
-  phone: string;
-  is_active: boolean;
-  created_at: Date;
-}
-
-// Career types
-export interface Career {
-  id: bigint;
-  name: string;
-}
-
-// Area types
-export interface Area {
-  id: bigint;
-  name: string;
-}
-
-// Interview types
-export interface Interview {
-  id: bigint;
-  user_id: string;
-  job_description: string;
-  resume: string;
-}
-
-// Conversation types
-export interface Conversation {
-  id: bigint;
-  interview_id: bigint;
-  //details: JSON
-}
-
 export interface SignUpData {
   email: string;
   password: string;
@@ -49,3 +6,4 @@ export interface SignUpData {
   institution_id: bigint;
   career_id: bigint;
 }
+
